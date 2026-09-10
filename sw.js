498ed6e16b72e5c6102db529754d30e9356f7640
gds3265 <elodie.jegou.gds65@reseaugds.com>
2026-09-10 11:57:00 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE='suivi-concours-v1118-20260910';
-const ASSETS=['./index.html?v=1118','./manifest.json?v=1118'];
+const CACHE='suivi-concours-v1119-20260910';
+const ASSETS=['./index.html?v=1119','./manifest.json?v=1119'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
-  if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).catch(()=>caches.match('./index.html?v=1118')));return}
+  if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).catch(()=>caches.match('./index.html?v=1119')));return}
   e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
 });
